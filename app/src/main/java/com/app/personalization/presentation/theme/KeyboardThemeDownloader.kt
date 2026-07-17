a4135f77820ad9cc421a518fa5209277c6eba434
CuongNV1 <cuongnv@cscmobi.com>
2026-07-17 09:01:36 +0700

    fix bugs

--- app/src/main/java/com/app/personalization/presentation/theme/KeyboardThemeDownloader.kt
+++ app/src/main/java/com/app/personalization/presentation/theme/KeyboardThemeDownloader.kt
@@ -80,6 +80,7 @@ object KeyboardThemeDownloader {
         try {
             val url = URL(urlStr)
             connection = url.openConnection() as HttpURLConnection
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
             connection.connectTimeout = 5000
             connection.readTimeout = 5000
             connection.connect()
--- app/src/main/java/com/app/personalization/presentation/widget/DownloadDialogFragment.kt
+++ app/src/main/java/com/app/personalization/presentation/widget/DownloadDialogFragment.kt
@@ -44,6 +44,7 @@ class DownloadDialogFragment : DialogFragment() {
             try {
                 val url = URL(downloadUrl)
                 val connection = url.openConnection() as HttpURLConnection
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
                 connection.connect()
 
                 if (connection.responseCode != HttpURLConnection.HTTP_OK) {
